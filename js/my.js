e1b0fc6a2ed06871567b126031cc79f9f2b3c21e
ellencurrin <ellen.c.currin@gmail.com>
2015-03-05 16:43:16 -0500

    first commit

---
+++ js/my.js
@@ -0,0 +1,43 @@
+
+function bisles() {
+    document.getElementById("gb").src = "images/bisles.png";
+}
+function gb1() {
+    document.getElementById("gb").src = "images/gb.png";
+}
+function england() {
+    document.getElementById("uk").src = "images/england.png";
+}
+function scotland() {
+    document.getElementById("uk").src = "images/scotland.png";
+}
+function wales() {
+    document.getElementById("uk").src = "images/wales.png";
+}
+function nireland() {
+    document.getElementById("uk").src = "images/nireland1.png";
+}
+function unitedkingdom() {
+    document.getElementById("uk").src = "images/unitedkingdom1.png";
+}
+function nireland2() {
+    document.getElementById("uk").src = "images/nireland1.png";
+}
+function gb2() {
+    document.getElementById("uk").src = "images/gb.png";
+}
+function nireland1() {
+    document.getElementById("ireland").src = "images/nireland1.png";
+}
+function sireland() {
+    document.getElementById("ireland").src = "images/sireland.png";
+}
+function ire() {
+    document.getElementById("ireland").src = "images/ireland.png";
+}
+function uk2() {
+    document.getElementById("ireland").src = "images/unitedkingdom1.png";
+}
+
+
+    
